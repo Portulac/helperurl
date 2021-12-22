83523b35f6027b4912c0c8a9957b370b6b8be3b2
Ekaterina Klyuzheva <e.klyuzheva@citrus-soft.ru>
2021-12-22 15:29:09 +0300

    Initial

---
+++ src/gsu/helperurl/HelperURL.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace gsu\helperurl;
+
+class HelperURL
+{
+	/**
+	 * @return mixed
+	 */
+	public function GetCurePageURL()
+	{
+		$pageURL = "http";
+		if (isset($_SERVER["HTTPS"]))
+		{
+			if ($_SERVER["HTTPS"] == "on")
+			{
+				$pageURL .= "s";
+			}
+		}
+		$pageURL .= "://";
+		if ($_SERVER["SERVER_PORT"] != "80")
+		{
+			$pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"];
+		}
+		else
+		{
+			$pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+		}
+
+		return $pageURL;
+	}
+}
